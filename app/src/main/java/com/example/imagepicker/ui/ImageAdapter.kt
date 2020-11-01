698abe3df5ecc381c31d5b6f9e833e05d0351ee6
Shlyankin <shlyankin123@gmail.com>
2020-11-01 13:32:20 +0400

    Animation and ImageLoading improvement

--- app/src/main/java/com/example/imagepicker/ui/ImageAdapter.kt
+++ app/src/main/java/com/example/imagepicker/ui/ImageAdapter.kt
@@ -1,21 +1,33 @@
 package com.example.imagepicker.ui
 
+import android.media.Image
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.example.imagepicker.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.subjects.PublishSubject
 import kotlinx.android.synthetic.main.item_image.view.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class ImageAdapter(
     private var uriList: List<Uri> = emptyList(),
     private var onClickListener: ((view: View, position: Int) -> Unit)? = null
-) : RecyclerView.Adapter<ImageViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val CAMERA_ITEM = 0
+        const val IMAGE_ITEM = 1
+    }
 
     private val expandSubject = PublishSubject.create<Boolean>()
 
@@ -24,28 +36,70 @@ class ImageAdapter(
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
-        ImageViewHolder(parent, expandSubject)
+    override fun getItemViewType(position: Int) = when (position) {
+        0 -> {
+            if (isExpanded) {
+                IMAGE_ITEM
+            } else {
+                CAMERA_ITEM
+            }
+        }
+        1 -> IMAGE_ITEM
+        else -> IMAGE_ITEM
+    }
 
-    override fun getItemCount() = uriList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = when (viewType) {
+        CAMERA_ITEM -> CameraViewHolder(parent, expandSubject)
+        IMAGE_ITEM -> ImageViewHolder(parent, expandSubject)
+        else -> ImageViewHolder(parent, expandSubject)
+    }
+
+    override fun getItemCount() = uriList.size+ if (isExpanded) 0 else 1
 
     var isExpanded = false
         private set
 
-    private val itemClickListener: ((view: View, position: Int) -> Unit)= { view: View, position: Int ->
-        isExpanded = !isExpanded
-        onClickListener?.invoke(view, position)
-        expandSubject.onNext(isExpanded)
+    private val itemClickListener: ((view: View, position: Int) -> Unit) =
+        { view: View, position: Int ->
+            isExpanded = !isExpanded
+            val scrollToPosition = position - if (isExpanded) 1 else 0
+            onClickListener?.invoke(view, scrollToPosition)
+            expandSubject.onNext(isExpanded)
+            if (isExpanded) {
+                notifyItemRemoved(0)
+            } else {
+                notifyItemInserted(0)
+            }
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (!isExpanded) {
+            if (holder is CameraViewHolder) {
+
+            } else if (holder is ImageViewHolder) {
+                holder.bind(uriList[position - 1], position, itemClickListener)
+            }
+        } else if (holder is ImageViewHolder){
+            holder.bind(uriList[position], position, itemClickListener)
+        }
     }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.bind(uriList[position], position, itemClickListener)
-    }
-
-    override fun onViewAttachedToWindow(holder: ImageViewHolder) {
+    override fun onViewAttachedToWindow(holder: RecyclerView.ViewHolder) {
         super.onViewAttachedToWindow(holder)
-        holder.changeSize(isExpanded)
+        if (holder is ImageViewHolder)
+            holder.changeSize(isExpanded)
     }
+}
+
+class CameraViewHolder(
+    parent: ViewGroup,
+    expandObservable: Observable<Boolean>
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.item_camera, parent, false)
+) {
+
 }
 
 class ImageViewHolder(
@@ -55,6 +109,8 @@ class ImageViewHolder(
     LayoutInflater.from(parent.context)
         .inflate(R.layout.item_image, parent, false)
 ) {
+
+    private var uri: Uri? = null
 
     init {
         expandObservable.subscribe { isExpanded ->
@@ -94,12 +150,17 @@ class ImageViewHolder(
         uri: Uri, position: Int,
         onClickListener: ((view: View, position: Int) -> Unit)? = null
     ) {
+        this.uri = uri
         itemView.run {
-            Glide.with(this).load(uri).into(image_view)
+            Glide.with(this).load(uri)
+                .apply(RequestOptions()
+                    .centerCrop()
+                    .format(DecodeFormat.PREFER_ARGB_8888)
+                    .override(600, 900)
+                ).into(image_view)
             setOnClickListener {
                 onClickListener?.invoke(it, position)
             }
         }
     }
-
 }
